e01853ff456aa2cf89a5d437e79d582dae6fe292
Karandeep Singh <karandeep@cgc.edu.in>
2026-05-03 12:39:08 +0530

    fix port binding for Railway

--- app.py
+++ app.py
@@ -441,5 +441,5 @@ def migrate3():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=False, host='0.0.0.0', port=port)
