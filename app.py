b5ca63489579acdb751b33a9024f2398f6c0e411
Karandeep Singh <karandeep@cgc.edu.in>
2026-05-09 11:24:38 +0530

    fix: students 500 + add superadmin event registrations route

--- app.py
+++ app.py
@@ -759,4 +759,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=False, host='0.0.0.0', port=port)s
+    app.run(debug=False, host='0.0.0.0', port=port)
